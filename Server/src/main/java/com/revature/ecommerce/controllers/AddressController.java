2bbec47034ce5db3458ebaa53f7d5d868c49793a
mmanultra <mmanultra2@gmail.com>
2024-01-10 12:07:17 -0500

    address controller

---
+++ Server/src/main/java/com/revature/ecommerce/controllers/AddressController.java
@@ -0,0 +1,35 @@
+package com.revature.ecommerce.controllers;
+
+import com.revature.ecommerce.entities.Address;
+import com.revature.ecommerce.services.AddressService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:4200", allowCredentials = "true")
+@RequestMapping(path = "/addresses")
+public class AddressController {
+
+    private final AddressService addressService;
+
+    @Autowired
+    public AddressController(AddressService addressService) {
+        this.addressService = addressService;
+    }
+
+    @GetMapping(path = "/{addressId}")
+    @ResponseStatus(HttpStatus.OK)
+    public Address getAddress(@PathVariable String addressId){
+        return addressService.findById(Integer.valueOf(addressId));
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.OK)
+    public Address addAddress(@RequestBody Address address){
+        return addressService.saveOrUpdate(address);
+    }
+
+
+}
