be269e8c02308c5e92cadbfb928fe67084a7e64d
mmanultra <mmanultra2@gmail.com>
2024-01-10 11:44:45 -0500

    find address by Id

--- Server/src/main/java/com/revature/ecommerce/services/AddressService.java
+++ Server/src/main/java/com/revature/ecommerce/services/AddressService.java
@@ -1,10 +1,14 @@
 package com.revature.ecommerce.services;
 
 import com.revature.ecommerce.entities.Address;
+import com.revature.ecommerce.entities.Seller;
 import com.revature.ecommerce.repositories.AddressRepository;
 import jakarta.transaction.Transactional;
+import org.hibernate.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @Transactional(Transactional.TxType.REQUIRED)
@@ -19,9 +23,19 @@ public class AddressService {
 
     public Address saveOrUpdate(Address address){ return addressRepository.save(address); }
 
-    public void deleteAddress(Address address) { addressRepository.delete(address) }
+    public void deleteAddress(Address address) { addressRepository.delete(address); }
 
     public void deleteAddress(Integer id) { addressRepository.deleteById(id); }
+
+    public Address findById(Integer id)
+    {
+        Optional<Address> found = addressRepository.findById(id);
+        if (found.isPresent()) {
+            return found.get();
+        } else {
+            throw new ObjectNotFoundException(Address.class, "Address");
+        }
+    }
 
 
 }
