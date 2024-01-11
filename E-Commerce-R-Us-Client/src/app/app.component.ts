44cf1aa8dbfc0bed6e67dc8da7888991476f3f7a
thein-than-hein <theinthan912@revature.net>
2024-01-11 13:01:45 -0800

    Add routes and components

--- E-Commerce-R-Us-Client/src/app/app.component.ts
+++ E-Commerce-R-Us-Client/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ProductsComponent } from './products/products.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavbarComponent],
+  imports: [CommonModule, RouterOutlet, NavbarComponent, ProductsComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
