977172a1dba16b49271636790866476bf4fd678a
chrisHebs23 <christopher.heberden@gmail.com>
2024-01-19 12:35:18 -0500

    added cart to application

--- E-Commerce-R-Us-Client/src/app/cart/cart.component.ts
+++ E-Commerce-R-Us-Client/src/app/cart/cart.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { RemoteService } from '../remote.service';
+import { CartItem } from '../dto/cart-item-dto';
 
 @Component({
   selector: 'app-cart',
   standalone: true,
   imports: [],
   templateUrl: './cart.component.html',
-  styleUrl: './cart.component.css'
+  styleUrl: './cart.component.css',
 })
 export class CartComponent {
+  open: Boolean;
+  cartItems: number[];
 
+  constructor(private remote: RemoteService) {
+    this.open = true;
+    this.cartItems = [1, 2, 3];
+  }
+
+  onOpen(): void {
+    console.log('hit');
+    this.open = !this.open;
+  }
 }
---
+++ E-Commerce-R-Us-Client/src/app/dto/cart-item-dto.ts
@@ -0,0 +1,3 @@
+export class CartItem {
+  constructor(public productName: string, public price: number) {}
+}
